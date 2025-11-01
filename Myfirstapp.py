554bd1b141bd2d87bedb800ca09420c07471677a
grandhegopal <grandhegopal@gmail.com>
2025-11-01 19:11:29 +0530

    Change data source from CSV to Excel file

--- Myfirstapp.py
+++ Myfirstapp.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 
 st.write("My first app Hello *world!*")
-df = pd.read_csv("https://docs.google.com/spreadsheets/d/1CEr_xj8NYEhyw43hjOhPuzh0b7lF009e7OgWNL6P6MU/edit?usp=drive_link")
+df =  pd.read_excel("GOPAL3073SLG_New.xlsx")
 st.line_chart(df)
